8d140539d87de37fb2ffb3046a62bce31dc7a39f
Rengeer <52525792+Rengeer@users.noreply.github.com>
2019-07-04 10:44:06 +0300

    Add files via upload

---
+++ Ship.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef ShipH
+#define ShipH
+
+class GameBoard;
+class GameBoardCell;
+
+/******************************************************************************************************
+*	ЙКЮЯЯ - ЙНПЮАКЭ
+******************************************************************************************************/
+class Ship {
+    public:
+        std::vector < GameBoardCell > v_desc;
+
+        Ship    ();
+        ~Ship   ();
+
+        // б астеп йнпюакъ гюмняхл онкъ, б йнрнпшу нм пюяонкюцюеряъ
+        void    Create(GameBoard& gameBoard, int size, int ind_x, int ind_y, bool horizontal);
+
+        // ТСМЙЖХЪ БШЯРПЕКЮ ОН ЙНПЮАПЧ, еякх йнпюакэ сахр, рн нйпсфюел ецн онкел хг осяршу онкеи
+        bool    Shoot(GameBoard& gameBoard, int ind_x, int ind_y, bool &isDestroy);
+};
+#endif
